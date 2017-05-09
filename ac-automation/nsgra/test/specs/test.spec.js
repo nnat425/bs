865cef78116ac75928038124531f216a94b19fed
Sajjad Hossain <sajjad@withpulp.com>
2017-05-09 16:08:04 -0400

    Maiden Voyage in ES6

--- ac-automation/nsgra/test/specs/test.spec.js
+++
@@ -1,8 +0,0 @@
-
-var HomePage = require('../../homePage.objects');
-describe('login form', function () {
-    it('should deny access with wrong creds', function () {
-        HomePage.open('');
-		browser.debug();
-    });
-});
