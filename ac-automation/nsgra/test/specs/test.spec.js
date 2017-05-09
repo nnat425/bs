4a863b044b5a856fa51df0d42215433bd69d7aca
Nat <NavraNat@Scholastic.com>
2017-05-09 15:12:03 -0400

    add ac-automation folder

---
+++ ac-automation/nsgra/test/specs/test.spec.js
@@ -0,0 +1,7 @@
+
+var HomePage = require('../../homePage.objects');
+describe('login form', function () {
+    it('should deny access with wrong creds', function () {
+        HomePage.open();
+    });
+});
