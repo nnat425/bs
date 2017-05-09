205e05288853258a7a323a61a597cfd1c59bcea2
Sajjad Hossain <sajjad@withpulp.com>
2017-05-09 16:00:12 -0400

    Maiden Voyage in ES6

---
+++ playground/test/specs/test.spec.js
@@ -0,0 +1,8 @@
+
+var HomePage = require('../../homePage.objects');
+describe('login form', function () {
+    it('should deny access with wrong creds', function () {
+        HomePage.open('');
+		browser.debug();
+    });
+});
